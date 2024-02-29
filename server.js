eaf3e754d9fb0a9bb035e7b1278d691688adef1a
KareemEmad52 <kareememad209@gmail.com>
2024-02-29 20:40:12 +0200

    modified deployment config

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import { AppError } from './src/utils/error.handler.js'
 
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = +process.env.PORT
 env.config()
 
 app.use('/uploads', express.static("uploads"))
